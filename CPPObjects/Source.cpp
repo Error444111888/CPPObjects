eb410a11093b5b339d8883397c23c90e2d818d04
error418 <error418@hotmail.com>
2018-11-02 05:14:54 -0700

    Initial Commit

---
+++ CPPObjects/Source.cpp
@@ -0,0 +1,30 @@
+// Objects - by Ray
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	//Declaring a Class
+	class Rectangle
+	{
+	public:
+		int 2;
+		int 5;
+	};
+
+
+
+
+
+
+
+
+	cin.get();
+}
+
